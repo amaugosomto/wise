f20e07c7524a4a8fd7921ab6384d9092888cf5c7
amaugosomto <amaugosomto@gmail.com>
2022-01-12 13:40:47 +0100

    finished login and register pages

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Header'
+import Head from 'next/head'
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,6 +9,12 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div>
+      <Head>
+        <title>Wise App</title>
+        <meta name="description" content="Wise App Sample App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Header />
       <main>{ children }</main>
     </div>

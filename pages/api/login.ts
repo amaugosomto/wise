bf07adc3700910abc3641d5b3d15af9ea80d337c
amaugosomto <amaugosomto@gmail.com>
2022-01-20 10:14:26 +0100

    bug fix

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -4,26 +4,31 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 var bcrypt = require('bcryptjs');
 
 async function handler( req: NextApiRequest, res: NextApiResponse ): Promise<void | NextApiResponse<any>> {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'method not allowed' });
-  }
-  const loginData: LoginDetails = JSON.parse(req.body);
-
-  const userExists = await prisma.userAccount.findUnique({
-    where: {
-      email: loginData.email
+  try {
+    if (req.method !== 'POST') {
+      return res.status(405).json({ message: 'method not allowed' });
     }
-  });
-  if (!userExists)
-    return res.status(404).json({message: 'email not found'});
-
-  const isCorrectPassword: boolean = bcrypt.compareSync(loginData.password, userExists.password);
-  if (!isCorrectPassword)
-    return res.status(401);
-
-  userExists.password = "";
-
-  res.json(userExists);
+    const loginData: LoginDetails = JSON.parse(req.body);
+  
+    const userExists = await prisma.userAccount.findUnique({
+      where: {
+        email: loginData.email
+      }
+    });
+    if (!userExists)
+      return res.status(404).json({message: 'email not found'});
+  
+    const isCorrectPassword: boolean = bcrypt.compareSync(loginData.password, userExists.password);
+    if (!isCorrectPassword)
+      return res.status(401);
+  
+    userExists.password = "";
+  
+    res.json(userExists);
+  } catch (error) {
+    res.status(500).json({error})
+  }
+  
 }
 
 export default handler;
